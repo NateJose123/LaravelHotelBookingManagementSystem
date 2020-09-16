3e4ef07eaa2fd755e1c85eb3a6017aadd0c9b2f5
Nathan Jose <nathan_jose@berea.edu>
2020-09-16 17:05:00 +0530

    Adding local project to Git

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Booking;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $bookings=DB::table('bookings')->get();
+       return view('bookings.index')
+              ->with('bookings', $bookings);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $users=DB::table('users')->get()->pluck('name', 'id')->prepend('none');
+        $rooms=DB::table('rooms')->get()->pluck('number', 'id');
+        return view('bookings.create')
+            ->with('users', $users)
+            ->with('rooms', $rooms);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Booking $booking)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ShowRoomsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/test', function() {return 'Goodbye';});
+
+Route::get('/rooms', \App\Http\Controllers\ShowRoomsController::class);
+
+Route::resource('bookings', \App\Http\Controllers\BookingController::class);
